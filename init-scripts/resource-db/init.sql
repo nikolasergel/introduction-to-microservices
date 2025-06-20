2c9a7822a04b88c1abc652fade23ff6cacf64a43
Mikalai Serhel <mikalai_serhel@epam.com>
2025-06-20 16:11:06 +0300

    Containerized overview task

---
+++ init-scripts/resource-db/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE resources (
+    id BIGSERIAL PRIMARY KEY,
+    data oid NOT NULL UNIQUE
+);
--- song-service/src/main/java/com/epam/songservice/entity/Song.java
+++ song-service/src/main/java/com/epam/songservice/entity/Song.java
@@ -3,6 +3,7 @@ package com.epam.songservice.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ import java.util.Objects;
 @Getter
 @Setter
 @Entity
+@Table(name = "songs")
 public class Song {
     @Id
     private Long id;
